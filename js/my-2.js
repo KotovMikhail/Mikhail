e937dd6dca57360a8cbf20b277d71f4c72ac4b34
Mikhail <mihail_kotov_86@mail.ru>
2018-07-01 12:35:54 +0300

    afdafd

---
+++ js/my-2.js
@@ -0,0 +1,7 @@
+var students = ["Абаканов Сергей", "Гагарин Матвей"];
+ if (students.length >= 3) {
+  document.write("Здесь как минимум 3 элемента");
+   }
+ else { 
+ 	document.write("Здесь меньше 3-х элемента") 
+}﻿
